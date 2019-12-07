e4f88f96f9c37c594e53837e1c510940b354d7ef
davidTortosa <36964300+davidTortosa@users.noreply.github.com>
2019-12-07 13:44:06 +0100

    Update functions.php

--- P3/my_groupThingsYouDontNeed/include/functions.php
+++ P3/my_groupThingsYouDontNeed/include/functions.php
@@ -380,8 +380,7 @@ function TYDN_modify_user(){
     <script type="text/javascript"  defer charset="utf-8">
 
     function mostrarFoto(file, imagen) {
-        console.log(file.width);
-        console.log(file.height);
+        console.log(file);
         var extensions = ['JPG','JPEG'];
         var ex = file.name.split(".");
         if(extensions.includes(ex[1].toUpperCase())){
