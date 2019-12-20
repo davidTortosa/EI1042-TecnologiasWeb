322dfcbe4e75ed99315d944dca701775a353e292
davidTortosa <36964300+davidTortosa@users.noreply.github.com>
2019-12-20 12:23:38 +0100

    Update functions.php

--- P3/my_groupThingsYouDontNeed/include/functions.php
+++ P3/my_groupThingsYouDontNeed/include/functions.php
@@ -59,6 +59,47 @@ function TYDN_Register_Form($MP_user , $user_email)
         <input type="submit" class="paracetamol" value="Enviar">
         <input type="reset" class="ibuprofeno" value="Deshacer">
     </form>
+
+    <script type="text/javascript" charset="utf-8" async="" defer="">
+
+        async function enviaForm(evento) {
+           try {
+              evento.preventDefault();
+              var imagen  = document.querySelector("#img_foto");
+              let url = evento.target.getAttribute("action")
+              let data = new FormData(evento.target);
+              let init = {
+                 url: url,
+                 method: 'post',
+                 body: data
+              };
+              let request0 = new Request(url, init);
+
+              const response = await fetch(request0);
+
+              if (!response.ok) {
+                 throw Error(response.statusText);
+              }   
+              const result = await response.text();
+              console.log('Tutto benne:', result);
+        evento.target.reset ();
+                    imagen.src = "";
+
+
+
+        window.alert("Usuario añadido con éxito. ¿Seguir añadiendo amigos?");
+           } catch (error) {
+              console.log(error);
+           }
+
+        }
+        if (document.forms.length > 0) {
+           document.forms[0].addEventListener("submit", function (event) {
+              enviaForm(event);
+
+           })
+        }
+    </script>
     <script type="text/javascript"  defer charset="utf-8">
 
     function mostrarFoto(file, imagen) {
